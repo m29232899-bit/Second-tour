9473ac87f586717d14b4f769d4c11dd8d26f6500
m29232899-bit <m29232899@gmail.com>
2026-09-20 16:54:29 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
 
-export const metadata = { title: "Second Tour" };
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body style={{margin:0}}>{children}</body>
     </html>
   );
 }
